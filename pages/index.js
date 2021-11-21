c08b79adc4a6317d9a27c6ea334afce21829895d
MasterDicc <90416870+MasterDicc@users.noreply.github.com>
2021-11-21 16:30:08 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,12 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
+window.onload = function() {
+    location.href = "https://www.javascripttutorial.net/";
+}
+
 export default function Home() {
+  
   return (
     <div className="container">
       <Head>
