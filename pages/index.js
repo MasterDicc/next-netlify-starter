794f579766ab3745e917a51a7e5d5d2dba7cc799
MasterDicc <90416870+MasterDicc@users.noreply.github.com>
2021-11-21 16:41:53 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,23 @@
-window.onload = function() {
-    location.href = "https://www.javascripttutorial.net/";
-}
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
 
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="Welcome to my app!" />
+        <p className="description">
+          Get started by editing <code>pages/index.js</code>
+        </p>
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
